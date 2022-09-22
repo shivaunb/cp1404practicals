53b622166aee322f4e5b22a1629b22ed3476d214
shivaunb <shivaun.bretz@my.jcu.edu.au>
2022-09-22 15:11:56 +1000

    Add Prac 01

---
+++ prac_01/menus.py
@@ -0,0 +1,31 @@
+"""
+Menu Pattern
+
+get name
+display menu
+get choice
+while choice != Q
+   if choice == H
+       display "hello" name
+   else if choice == G
+       display "goodbye" name
+   else
+       display invalid message
+   display menu
+   get choice
+display finished message
+"""
+
+name = input("Enter name: ").title()
+print("(H)ello \n(G)oodbye \n(Q)uit")
+choice = input(">>> ").upper()
+while choice != "Q":
+    if choice == "H":
+        print(f"Hello {name}")
+    elif choice == "G":
+        print(f"Goodbye {name}")
+    else:
+        print("Invalid choice")
+    print("(H)ello \n(G)oodbye \n(Q)uit")
+    choice = input(">>> ").upper()
+print("Finished.")
