94cd2cb3015d5ac37b2d236b4b5250819b04b77a
shivaunb <shivaun.bretz@my.jcu.edu.au>
2022-09-24 10:46:17 +1000

    Refactor password check to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,9 +1,40 @@
-MINIMUM = 8
-password = input("Enter Password: ")
-password_length = len(password)
-while password_length < MINIMUM:
-    print("Invalid Password")
+"""
+CP1404 - Practical 2
+Get a password with minimum length and display asterisks
+"""
+
+MINIMUM_LENGTH = 5
+
+
+def part_one():
+    """Get eligible password and print asterisks."""
     password = input("Enter Password: ")
-    password_length = len(password)
-for i in range(password_length):
-    print("*", end="")
+    while len(password) < MINIMUM_LENGTH:
+        print("Password not long enough. Try again")
+        password = input("Enter Password: ")
+    for i in range(len(password)):
+        print("*", end="")
+
+
+# part_one()
+
+def main():
+    """Get eligible password and print asterisks using functions."""
+    password = get_password(MINIMUM_LENGTH)
+    print_asterisks(password)
+
+
+def get_password(minimum_length):
+    password = input("Enter Password: ")
+    while len(password) < MINIMUM_LENGTH:
+        print("Password needs to be longer than {} characters. Try again".format(minimum_length))
+        password = input("Enter Password: ")
+    return password
+
+
+def print_asterisks(password):
+    for i in range(len(password)):
+        print("*", end="")
+
+
+main()
