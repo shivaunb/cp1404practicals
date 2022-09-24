3f7f7912ee8dab3ab21b5bd681a378c7840353dc
shivaunb <shivaun.bretz@my.jcu.edu.au>
2022-09-24 11:36:46 +1000

    Refactor score program using functions and add random score generator

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,29 +1,28 @@
 """
-CP1404/CP5632 - Practical
-Broken program to determine score status
+CP1404 - Practical 2
+Program to determine Score status
 """
+import random
 
-# # TODO: Fix this!
-#
-# score = float(input("Enter score: "))
-# if score < 0:
-#     print("Invalid score")
-# else:
-#     if score > 100:
-#         print("Invalid score")
-#     if score > 50:
-#         print("Passable")
-#     if score > 90:
-#         print("Excellent")
-# if score < 50:
-#     print("Bad")
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score > 50:
-        print("Passable")
-else:
-    print("Bad")
+def main():
+    """Get user score and generate random score and display both score status """
+    score = float(input("Enter score: "))
+    print("Your score is {}".format((determine_score_status(score))))
+    random_score = random.randint(0, 100)
+    print("The randomly chosen score is {} and it is {}".format(random_score, (determine_score_status(random_score))))
+
+
+def determine_score_status(score):
+    """Determine score status of given score."""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
