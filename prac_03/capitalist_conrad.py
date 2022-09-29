b48fca6c4085c797ae7b8b7c79aaebd1a52afdb1
shivaunb <shivaun.bretz@my.jcu.edu.au>
2022-09-29 20:40:24 +1000

    Modify capitalist_conrad program to write to stock_price.txt

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -9,15 +9,17 @@ The price should be displayed to the nearest cent (e.g. $33.59, not $33.59182329
 """
 import random
 
-MAX_INCREASE = 0.1  # 10%
+MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05  # 5%
-MIN_PRICE = 0.01
-MAX_PRICE = 1000.0
+MIN_PRICE = 1.0
+MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
+OUTPUT_FILE = "stock_price.txt"
 
 number_of_days = 0
 price = INITIAL_PRICE
-print("${:,.2f}".format(price))
+out_file = open(OUTPUT_FILE, 'w')
+print("Starting price: ${:,.2f}".format(price), file=out_file)
 
 while price >= MIN_PRICE and price <= MAX_PRICE:
     price_change = 0
@@ -34,4 +36,6 @@ while price >= MIN_PRICE and price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print("On day {} price is: ${:,.2f}".format(number_of_days, price))
+    print("On day {} price is: ${:,.2f}".format(number_of_days, price), file=out_file)
+
+out_file.close()
