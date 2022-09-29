6884e317ed8ea4279d54c0df71558fc08e5c364b
shivaunb <shivaun.bretz@my.jcu.edu.au>
2022-09-29 16:10:58 +1000

    Modified string_formatting and randoms programs to include formatting

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -2,10 +2,12 @@
 CP1404 - Practical
 Using Random Functions
 """
-#Line 1. The smallest number that showed was 5 and the largest was 19
-#Line 2. The smallest number that showed was 3 and the largest number was 9. As it is going up in increments of 2 starting at 3, 4 would not be produced (odd numbers between 3 & 10 only)
-#Line 3. The smallest number that could have showed is 2.5 and the largest is 4.9999999999999999
+# Line 1. The smallest number that showed was 5 and the largest was 19
+# Line 2. The smallest number that showed was 3 and the largest number was 9. As it is going up in increments of 2
+# starting at 3, 4 would not be produced (odd numbers between 3 & 10 only)
+# Line 3. The smallest number that could have showed is 2.5 and the largest is 4.9999999999999999
 
-#code to produce random number between 1 and 100 inclusive
+# code to produce random number between 1 and 100 inclusive
 import random
-print(random.randint(0,101))
+
+print(random.randint(0, 101))
