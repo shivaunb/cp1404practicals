10c9a6209a8d1d48f3e6283cc502f85e25766020
shivaunb <shivaun.bretz@my.jcu.edu.au>
2022-09-30 20:16:17 +1000

    Modify files program to add question two

--- prac_03/files.py
+++ prac_03/files.py
@@ -4,8 +4,17 @@ Experience with different ways to read files
 """
 
 # Question 1
-user_name = input("Enter your name: ")
+user_name = input("Enter your name: ").title()
 out_file = open("name.txt", "w")
 print(user_name, file=out_file)
 out_file.close()
+
+# Question 2
+in_file = open("name.txt", "r")
+user_name = in_file.read().strip()
+in_file.close()
+print(f"Your name is {user_name}")
+
+# Question 3
+
 
