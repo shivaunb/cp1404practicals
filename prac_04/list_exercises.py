bd4213382b6285d0be9fa24e56e377f5333cdac2
shivaunb <shivaun.bretz@my.jcu.edu.au>
2022-10-07 19:34:34 +1000

    Modify list_exercises to add username task two

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,7 +1,10 @@
 """
 CP1404 Practical
 Program that gets numbers from user and provides information about these numbers
+Program that checks if entered username is in a list of authorised users
 """
+
+# 1. Information about numbers entered by user
 TOTAL_NUMBER = 5
 
 numbers = []
@@ -12,5 +15,15 @@ print(f"The first number is {numbers[0]}")
 print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
-print(f"The average of the numbers is {sum(numbers)/len(numbers)}")
+print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
 
+# 2. Authorised usernames
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Enter username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
