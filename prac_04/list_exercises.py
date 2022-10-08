7ca38d50d20425c0553e62f23d1811c2000b9776
shivaunb <shivaun.bretz@my.jcu.edu.au>
2022-10-08 15:04:07 +1000

    Modify list_exercises to change comments

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -17,7 +17,7 @@ print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
 
-# 2. Authorised usernames
+# 2. Woefully inadequate security checker - Authorised usernames
 
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
