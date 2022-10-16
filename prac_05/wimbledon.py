a17774dc411e60aa76507eb180c7d42b45c07748
shivaunb <shivaun.bretz@my.jcu.edu.au>
2022-10-16 12:48:59 +1000

    Modify wimbledon program to add process_records function and return dictionary

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -4,11 +4,15 @@ Wimbledon - Write a program to display information regarding Wimbledon Gentlemen
 """
 
 FILENAME = "wimbledon.csv"
+COUNTRY_INDEX = 1
+CHAMPION_INDEX = 2
 
 
 def main():
     records = get_information(FILENAME)
-    print(records)
+    champion = process_records(records)
+    print(records)  # test list
+    print(champion)  # test dictionary
 
 
 def get_information(filename):
@@ -19,6 +23,16 @@ def get_information(filename):
             parts = line.strip().split(",")
             records.append(parts)
     return records
+
+
+def process_records(records):
+    champion_to_amount = {}
+    for record in records:
+        try:
+            champion_to_amount[record[CHAMPION_INDEX]] += 1
+        except KeyError:
+            champion_to_amount[record[CHAMPION_INDEX]] = 1
+    return champion_to_amount
 
 
 main()
