e8b3fee14420a9e9488796ad001d7c934b275ec0
shivaunb <shivaun.bretz@my.jcu.edu.au>
2022-10-16 13:15:54 +1000

    Add display_records function to wimbledon program

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,6 +1,9 @@
 """
 CP1404 Practical
 Wimbledon - Write a program to display information regarding Wimbledon Gentlemen Singles Champions
+
+Estimated time: 45 minutes
+Actual time: 62 minutes
 """
 
 FILENAME = "wimbledon.csv"
@@ -11,9 +14,10 @@ CHAMPION_INDEX = 2
 def main():
     records = get_information(FILENAME)
     champion_to_amount, countries = process_records(records)
-    print(records)  # test list
-    print(champion_to_amount)  # test dictionary
-    print(countries)  # test list
+    display_records(champion_to_amount, countries)
+    # print(records)  # test list
+    # print(champion_to_amount)  # test dictionary
+    # print(countries)  # test list
 
 
 def get_information(filename):
@@ -36,6 +40,16 @@ def process_records(records):
         except KeyError:
             champion_to_amount[record[CHAMPION_INDEX]] = 1
     return champion_to_amount, countries
+
+
+def display_records(champion_to_amount, countries):
+    print(f"Wimbledon Champions:")
+    for name, amount in champion_to_amount.items():
+        print(f"{name} {amount}")
+
+    print(f"These {len(countries)} countries have won Wimbledon: ")
+    countries = sorted(countries)
+    print(", ".join(countries))
 
 
 main()
