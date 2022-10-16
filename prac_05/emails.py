77779e6cfe05aa22206d9a9b1091c6b3d5b2caf3
shivaunb <shivaun.bretz@my.jcu.edu.au>
2022-10-16 11:06:50 +1000

    Modify emails program to add get_name_from_email function

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,11 +3,30 @@ CP1404 Practical
 Emails - store user email and name in a dictionary
 
 Estimated time: 40 minutes
-Actual time:
+Actual time: 45 minutes
 """
 
-email_to_name = {}
-email = input("Email: ")
-name = email.split("@")
-print(name[0].split("."))
 
+def main():
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        name = get_name_from_email(email)
+        validity = input(f"Is your name {name}? (Y/n) ").upper()
+        if validity != "Y" and validity != "":
+            name = input("Name: ").title()
+        email_to_name[email] = name
+        email = input("Email: ")
+
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+
+def get_name_from_email(email):
+    prefix = email.split("@")
+    parts = (prefix[0].split("."))
+    name = " ".join(parts).title()
+    return name
+
+
+main()
