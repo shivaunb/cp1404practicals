f463f8eb49777f2ab65d5ba57e2b74845877e85f
shivaunb <shivaun.bretz@my.jcu.edu.au>
2022-11-02 20:48:11 +1000

    Modify guitar program to add class

---
+++ prac_06/guitar.py
@@ -0,0 +1,30 @@
+"""
+CP1404 Practical
+Guitar Class
+Estimated time: 60 minutes
+Actual time:
+"""
+CURRENT_YEAR = 2022
+VINTAGE_AGE = 50
+
+
+class Guitar:
+    """Guitar class that stores the details of a guitar."""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a guitar."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return the details of a guitar as a string."""
+        return f"{self.name} ({self.year}) : {self.cost}"
+
+    def get_age(self):
+        """Get age of guitar based on current year."""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Determine is a guitar is vintage based on its age."""
+        return self.get_age() >= VINTAGE_AGE
